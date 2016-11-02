ee5f8372b3159b4223bd7c40461244dad256d200
Sylvia Kinosian <Sylvia@Sylvias-MacBook-Air.local>
2016-11-02 11:44:05 -0600

    again

--- terrain.R
+++ terrain.R
@@ -1,4 +1,4 @@
-#1: create matrix with odd dimensions
+#create matrix with odd dimensions
 
 odd.matrix<- function(x){
 
@@ -8,24 +8,23 @@ odd.matrix<- function(x){
 	}
 
 	#create a matrix with odd dimensions		
-	m  <- matrix(nrow = x, ncol = x)
-
-	return(m)
-	kalimdor <- m
+	mat  <- matrix(nrow = x, ncol = x)
+	
+	#initilize random values for each corner
+	mat[1, 1] <- rnorm(1, rnorm(1), runif(1, min=0))
+	mat[1, ncol(mat)] <- rnorm(1, rnorm(1), runif(1, min=0))
+	mat[nrow(mat), 1] <- rnorm(1, rnorm(1), runif(1, min=0))
+	mat[nrow(mat), ncol(mat)] <- rnorm(1, rnorm(1), runif(1, min=0))
+	
+	return(mat)
 }
 
-kalimdor <- odd.matrix(4)
+# create diamond step f(x)
 
-#2: initilize random values for each corner
+diamond.step <- function(x){
+	m <- mat(x)
+	
 
-kalimdor[1, 1] <- rnorm(1, rnorm(1), runif(1, min=0))
-kalimdor[1, ncol(kalimdor)] <- rnorm(1, rnorm(1), runif(1, min=0))
-kalimdor[nrow(kalimdor), 1] <- rnorm(1, rnorm(1), runif(1, min=0))
-kalimdor[nrow(kalimdor), ncol(kalimdor)] <- rnorm(1, rnorm(1), runif(1, min=0))
-
-#3: create diamond f(x)
-
-diamond.step <- function(){
 }
 
 #4: create square f(x)
