d8cfda5a85a4a604ea5723d9f51c27aba24e1df0
Sylvia Kinosian <Sylvia@Sylvias-MacBook-Air.local>
2016-11-02 11:30:41 -0600

    terrain

--- terrain.R
+++ terrain.R
@@ -1,15 +1,36 @@
 #1: create matrix with odd dimensions
-#2: initialize random values for each corner
 
-terrain <- matrix(nrow = 7, ncol = 7)
+odd.matrix<- function(x){
 
-terrain[1,1] <- rnorm(1, rnorm(1), runif(1, min=0))
-terrain[1,7] <- rnorm(1, rnorm(1), runif(1, min=0))
-terrain[7,1] <- rnorm(1, rnorm(1), runif(1, min=0))
-terrain[7,7] <- rnorm(1, rnorm(1), runif(1, min=0))
+	#if x is an even number, f(x) adds 1 to make it odd
+	if (x %% 2 == 0){
+	x <- x+1
+	}
+
+	#create a matrix with odd dimensions		
+	m  <- matrix(nrow = x, ncol = x)
+
+	return(m)
+	kalimdor <- m
+}
+
+kalimdor <- odd.matrix(4)
+
+#2: initilize random values for each corner
+
+kalimdor[1, 1] <- rnorm(1, rnorm(1), runif(1, min=0))
+kalimdor[1, ncol(kalimdor)] <- rnorm(1, rnorm(1), runif(1, min=0))
+kalimdor[nrow(kalimdor), 1] <- rnorm(1, rnorm(1), runif(1, min=0))
+kalimdor[nrow(kalimdor), ncol(kalimdor)] <- rnorm(1, rnorm(1), runif(1, min=0))
 
 #3: create diamond f(x)
 
+diamond.step <- function(){
+}
+
 #4: create square f(x)
+
+square.step <- function(){
+}
 
 #5: f(x) to fill matrix
