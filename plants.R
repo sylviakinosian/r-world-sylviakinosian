2803cd0e73cbe108c1510e873f8f20af464a415c
Sylvia Kinosian <Sylvia@Sylvias-MacBook-Air.local>
2016-11-07 12:16:50 -0700

    plants

--- plants.R
+++ plants.R
@@ -5,8 +5,7 @@
 #when in competition with another). You’re going to use the stop function to ‘raise’ an error if those 
 #conditions aren’t met
 
-comp.mat <- matrix(data=NA, nrow=3, ncol=3)
-
+#class for setting up plants to use
 setup.plants <- function(repro, survival, comp.mat, names=NULL){
   #if the user doesnt give me names, make names be a,b,c.. for as many entries as there are in repro
   if(is.null(names))
@@ -16,6 +15,7 @@ setup.plants <- function(repro, survival, comp.mat, names=NULL){
   if(length(repro) != length(survival))
     stop("Reproduction and survival parameters needed for all species")
   #more tests...which?
+  
   #give names for each variable for each plant
   repro <- setNames(repro, names)
   surv <- setNames(survival, names)
@@ -25,3 +25,17 @@ setup.plants <- function(repro, survival, comp.mat, names=NULL){
   class(output) <- "plants"
   return(output)
 }
+
+#inputs for the setup.plants function/class
+repro <- c(0.2,0.4,0.6)
+survival <- c(0.8,0.6,0.2)
+comp.mat <- matrix(data=NA, nrow=3, ncol=3)
+comp.mat[1,1] <- 0.5
+comp.mat[1,2] <- 0.3
+comp.mat[1,3] <- 0.8
+comp.mat[2,1] <- 0.3
+comp.mat[2,2] <- 0.5
+comp.mat[2,3] <- 0.1
+comp.mat[3,1] <- 0.8
+comp.mat[3,2] <- 0.1
+comp.mat[3,3] <- 0.5
